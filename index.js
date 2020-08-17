877f02eb1663b7ef834152d81aa7391fda38abdd
Trend20 <enockomondi305@gmail.com>
2020-08-17 15:24:06 -0400

    routes

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.get('/api/courses', (req, res) =>{
 });
 
 app.get('/api/courses/:id', (req, res) =>{
-   
+   const course = courses.find(c =>c.id === parseInt(req.params.id));
+   if(!course) res.status(404).send('Course Not Found');
+   res.send(course);
 });
 
 // port
